53fdeabfd51e60ba6f7063286edd1f4a5dfec0cb
Nick Cheng <nicholascheng25@gmail.com>
2021-04-03 15:02:23 +1100

    graphql project

---
+++ index.js
@@ -0,0 +1,28 @@
+const { dotenv } = require('dotenv').config()
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'Keystone101';
+const adapterConfig = { mongoUri: process.env.MONGO_URI };
+
+
+/**
+ * You've got a new KeystoneJS Project! Things you might want to do next:
+ * - Add adapter config options (See: https://keystonejs.com/keystonejs/adapter-mongoose/)
+ * - Select configure access control and authentication (See: https://keystonejs.com/api/access-control)
+ */
+
+const listSchema = require('./lists/Post')
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+  cookieSecret: process.env.COOKIE_SECRET,
+});
+
+keystone.createList('Post', listSchema)
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp(), new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true })],
+};
